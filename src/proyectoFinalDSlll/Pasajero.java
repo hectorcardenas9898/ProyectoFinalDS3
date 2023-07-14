685d66820b07fd7c1b07a627ac7bc3414bff482a
hectorcardenas9898 <127465512+hectorcardenas9898@users.noreply.github.com>
2023-07-14 13:14:32 -0500

    prueba clase pasajero

--- src/proyectoFinalDSlll/Pasajero.java
+++ src/proyectoFinalDSlll/Pasajero.java
@@ -5,10 +5,10 @@ import javax.swing.JOptionPane;
 public class Pasajero {
 
 	private String nombre;
-	private String identificacion;
+	private int identificacion;
 	private String nacionalidad;
 
-	public Pasajero(String nombre, String identificacion, String nacionalidad) {
+	public Pasajero(String nombre, int identificacion, String nacionalidad) {
 
 		this.nombre = nombre;
 		this.identificacion = identificacion;
